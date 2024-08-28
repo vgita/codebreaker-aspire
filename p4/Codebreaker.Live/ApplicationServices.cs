166d1c919e15256cb8ef5fdca0ac229ee6a060c9
vgita <22777783+vgita@users.noreply.github.com>
2024-08-28 19:25:30 +0300

    setup p4

---
+++ p4/Codebreaker.Live/ApplicationServices.cs
@@ -0,0 +1,25 @@
+﻿namespace Codebreaker.Live;
+
+public static class ApplicationServices
+{
+    public static void AddApplicationServices(this IHostApplicationBuilder builder)
+    {
+        var signalRBuilder = builder.Services.AddSignalR()
+            .AddMessagePackProtocol();
+
+        if (Environment.GetEnvironmentVariable("StartupMode") != "OnPremises")
+        { 
+            signalRBuilder.AddNamedAzureSignalR("signalr");
+        }
+    }
+
+    public static WebApplication MapApplicationEndpoints(this WebApplication app, ILogger logger)
+    {
+        // map REST endpoints
+        app.MapLiveGamesEndpoints(logger);
+
+        // map SignalR hub
+        app.MapHub<LiveHub>("/livesubscribe");
+        return app;
+    }
+}
